c9380855ccf30da54a577cf9c817ba33a69c211d
Dan Miller <dmm4613@gmail.com>
2019-04-01 15:05:04 -0400

    added some basic urls to our project

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Deck(models.Model):
+    card = models.ForeignKey('Card', on_delete_model=CASCADE)
+
+
+
+
+
+
+
+
+class Game(models.Model):
+    deck =     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Card(models.Model):
