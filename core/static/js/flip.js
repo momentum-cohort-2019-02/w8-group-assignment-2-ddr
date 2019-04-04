85a4b9eb62ad74fd3dbcec301dd584c83c02133d
Dan Miller <dmm4613@gmail.com>
2019-04-04 16:45:20 -0400

    our cards flip

--- core/static/js/flip.js
+++ core/static/js/flip.js
@@ -1,4 +1,8 @@
-const card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-});
+// card flip functionality found at https://codepen.io/desandro/pen/LmWozd
+
+const cards = document.querySelectorAll('.card')
+for (let card of cards){
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped')
+  });
+}
